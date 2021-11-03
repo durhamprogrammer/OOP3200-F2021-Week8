839b54f6dc847e9dc9de150e68fab0acb401751c
Tom Tsiliopoulos <tsiliopoulos@hotmail.com>
2021-11-03 11:44:27 -0400

    Added user input

--- src/Program.java
+++ src/Program.java
@@ -1,10 +1,30 @@
+import java.util.Scanner;
+
+/**
+ * @author Tom Tsiliopoulos
+ * @studentID 100100100
+ * @date November 3, 2021
+ *
+ */
+
 public class Program
 {
+    /**
+     * This function returns the square of the value parameter
+     *
+     * @param value a floating point number to be sqaured
+     * @return returns the square of value
+     */
     public static float square(float value)
     {
         return value * value;
     }
 
+    /**
+     * The entry point for our program
+     *
+     * @param args an array of command line parameters
+     */
     public static void main(String[] args)
     {
         System.out.println("Hello, World!");
@@ -12,9 +32,20 @@ public class Program
         float valueToSquare = 5;
         float squaredValue = square(valueToSquare);
 
+        // example of output
         System.out.println("The Squared Value is: " + squaredValue);
 
-        Person person = new Person("Tom", 30);
+        // Step 1. for input we need to create a Scanner object
+        Scanner inputObject = new Scanner(System.in);
+        System.out.print("Enter your name: ");
+        // Step 2. use the Scanner object to get the "next" line of data in this case a String value
+        String name = inputObject.nextLine();
+
+        System.out.print("Enter your age: ");
+        int age = inputObject.nextInt();
+
+        Person person = new Person(name, age);
         person.SaysHello();
+
     }
 }
